b3759a4c7d7c9ca2fad48b16c66d28dcfd87e6b8
Antonio Lorusso <mitictonylor@gmail.com>
2020-04-03 13:01:52 +0100

    add Sqlrunner.rb

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,1 +1,15 @@
+require('PG')
 
+class Sqlrunner
+
+  def self.run(sql,values =[])
+    begin
+      db = PG.connect({dbname: 'book_shop', host: 'localhost'})
+      db.prepare('query',sql)
+      result = db.exec_prepared('query',values)
+    ensure
+      db.close() if db != nil
+    end
+    return result
+  end
+end
