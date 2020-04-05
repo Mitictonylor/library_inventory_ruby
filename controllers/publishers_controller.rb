2aa65777ff2dfe7653c0dd07d7db205428a98dd2
Antonio Lorusso <mitictonylor@gmail.com>
2020-04-05 13:14:59 +0100

    add the publishers route

--- controllers/publishers_controller.rb
+++ controllers/publishers_controller.rb
@@ -1,1 +1,10 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' ) if development?
+require_relative( '../models/publisher.rb' )
+require_relative('../models/book.rb')
+also_reload('../models/*')
 
+get '/publishers' do
+  @publishers = Publisher.all()
+erb(:"publishers/index")
+end
