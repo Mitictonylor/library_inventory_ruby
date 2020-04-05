878ddfe7212e397e2395f96d2d09b34df749ebc9
Antonio Lorusso <mitictonylor@gmail.com>
2020-04-05 21:00:20 +0100

    add genres/new plus the post

--- controllers/genres_controller.rb
+++ controllers/genres_controller.rb
@@ -9,8 +9,18 @@ get '/genres' do
   erb ( :"genres/index" )
 end
 
+get '/genres/new' do
+  erb(:"genres/new")
+end
+
 get '/genres/:id' do
   id = params['id'].to_i
  @books = Genre.all_the_book_by_genre(id)
   erb( :"genres/show" )
 end
+
+  post '/genres' do
+    new_genre = Genre.new(params)
+    new_genre.save
+    erb(:"genres/create")
+  end
